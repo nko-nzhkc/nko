8b1df115124a4a5cf7b57ce169051c5ac6c78e36
Olga Karpova <tordvist@yandex.ru>
2024-04-22 18:37:56 +0300

    Add SMTP backend

--- backend/donations/signals.py
+++ backend/donations/signals.py
@@ -2,6 +2,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.core.mail import send_mail
 from .models import Donation
+from django.conf import settings
 
 
 @receiver(post_save, sender=Donation)
@@ -10,7 +11,7 @@ def send_donation_email(sender, instance, created, **kwargs):
         send_mail(
             "Thank you for your donation!",
             "Thank you for your donation!",
-            "from@example.com",
+            settings.EMAIL_HOST_USER,
             [instance.email],
             fail_silently=False,
         )
