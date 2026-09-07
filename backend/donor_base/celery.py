81fd1498c63eed4137c0659ac9baaadcac01d13f
Марат <kabardokov.m@yandex.ru>
2026-09-08 01:05:56 +0300

    Перевел на пакетный менеджер poetry. Пока что без бампа версий. Запуск проекта через poetry работает - создается виртуальное окружение, все зависимости подтягиваются. Пока что все библиотеки в тех же версиях, что и были

--- backend/donor_base/celery.py
+++ backend/donor_base/celery.py
@@ -1,12 +1,14 @@
-import django
 import os
 import logging
+
 from celery import Celery
 
 logger = logging.getLogger("Celery_logger")
 logger.setLevel(logging.INFO)
+
 handler = logging.FileHandler("celery.log")
 handler.setLevel(logging.INFO)
+
 formatter = logging.Formatter(
     "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
@@ -14,8 +16,11 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "donor_base.settings")
-django.setup()
+
 app = Celery("donor_base")
+
 app.config_from_object("django.conf:settings", namespace="CELERY")
+
 app.autodiscover_tasks()
+
 app.conf.broker_connection_retry_on_startup = True
