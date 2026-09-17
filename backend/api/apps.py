6de513f8df017d1c3f3c2c107afd27aeebdd174f
Марат Кабардоков <kabardokov.m@yandex.ru>
2026-09-18 00:22:20 +0300

    Постгрес поднят до 18 версии. Подправлены компоузы оба (изменение в маунте для БД из-за 18 версии относительно 16). Также перенес настройки poetry в pyproject.toml в корне проекта и poetry.lock также в корень перенесен. Соответственно этому отредактирован докерфайл

--- backend/api/apps.py
+++ backend/api/apps.py
@@ -8,5 +8,5 @@ class ApiConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "api"
 
-    def ready(self):
-        import api.signals  # noqa: F401
+    # def ready(self):
+    #     import api.signals  # noqa: F401
