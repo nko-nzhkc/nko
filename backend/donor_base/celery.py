81d511610a22b62cf8641c0b9c19d2ed4d249cc8
Марат <kabardokov.m@yandex.ru>
2026-09-19 16:04:40 +0300

    Вернул django.setup() в celery.py

--- backend/donor_base/celery.py
+++ backend/donor_base/celery.py
@@ -1,14 +1,12 @@
+import django
 import os
 import logging
-
 from celery import Celery
 
 logger = logging.getLogger("Celery_logger")
 logger.setLevel(logging.INFO)
-
 handler = logging.FileHandler("celery.log")
 handler.setLevel(logging.INFO)
-
 formatter = logging.Formatter(
     "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
@@ -16,11 +14,8 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "donor_base.settings")
-
+django.setup()
 app = Celery("donor_base")
-
 app.config_from_object("django.conf:settings", namespace="CELERY")
-
 app.autodiscover_tasks()
-
 app.conf.broker_connection_retry_on_startup = True
